654cf50312953ed46961b1ae88e6e43f3fece0a1
ignite <ignite@kirkclarke.com>
2014-08-13 23:05:28 -0400

    Added environment variables to heroku through CLI and edited carrierwave.rb to reference them

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,8 +12,8 @@ CarrierWave.configure do |config|
  
     # Configuration for Amazon S3
     :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET'],
+    :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
     :region                => ENV['S3_REGION']
   }
  
