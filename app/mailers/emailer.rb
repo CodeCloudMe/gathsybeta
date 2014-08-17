5a390f2a18b14760aa394e5d6c5004645a4db57a
ignite <ignite@kirkclarke.com>
2014-08-17 02:37:06 -0400

    minor css tweaks, and changed urls to gathsy.com

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,38 +1,38 @@
 class Emailer < ActionMailer::Base
-  default :from => "notifications@http://gathsykirk.herokuapp.com"
+  default :from => "notifications@http://gathsy.com"
  
   def welcome_email(user)
     @user = user
-    @url  = "http://gathsykirk.herokuapp.com"
-    @from = 'info@gathsykirk.herokuapp.com'
+    @url  = "http://gathsy.com"
+    @from = 'info@gathsy.com'
     mail(:to => user.email, :subject => "Welcome to Gathsy", :from => @from)
   end
 
    def new_space_email(user)
     @user = user
-    @url  = "http://gathsykirk.herokuapp.com"
-    @from = 'info@gathsykirk.herokuapp.com'
+    @url  = "http://gathsy.com"
+    @from = 'info@gathsy.com'
     mail(:to => user.email, :subject => "<div style='font-size:20px'>You've created a new space on Gathsy</div>", :from => @from)
   end
 
   def book_email(user)
     @user = user
-    @url  = "http://gathsykirk.herokuapp.com"
-    @from = 'info@gathsykirk.herokuapp.com'
+    @url  = "http://gathsy.com"
+    @from = 'info@gathsy.com'
     mail(:to => user.email, :subject => "New Booking Created", :from => @from)
   end
 
   def approve_email(user)
     @user = user
-    @url  = "http://gathsykirk.herokuapp.com"
-    @from = 'info@gathsykirk.herokuapp.com'
+    @url  = "http://gathsy.com"
+    @from = 'info@gathsy.com'
     mail(:to => user.email, :subject => "Booking Approved", :from => @from)
   end
 
   def decline_email(user)
     @user = user
-    @url  = "http://gathsykirk.herokuapp.com"
-    @from = 'info@gathsykirk.herokuapp.com'
+    @url  = "http://gathsy.com"
+    @from = 'info@gathsy.com'
     mail(:to => user.email, :subject => "Booking Declined", :from => @from)
   end
 
