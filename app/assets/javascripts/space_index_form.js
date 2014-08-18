97a9b839df113fe3fb38e48b80d8a32ef2bf40d0
ignite <ignite@kirkclarke.com>
2014-08-17 21:01:12 -0400

    testing filter js edit, with submit return

--- app/assets/javascripts/space_index_form.js
+++ app/assets/javascripts/space_index_form.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
-  $("#space-index-filter-form").find(":input").each(function(){
-    $(this).change(function(){
-      $("#space-index-filter-form").trigger('click');
-    });
-  });
+  // $("#space-index-filter-form").find(":input").each(function(){
+  //   $(this).change(function(){
+  //     $("#space-index-filter-form").trigger('click');
+  //   });
+  // });
 
-	// $("#space-index-sidebar-filter-button").on("click", function(){
-	// 	$("#space-index-filter-form").submit();
-	// });
+	$("#space-index-sidebar-filter-button").on("click", function(){
+		$("#space-index-filter-form").trigger('click');
+	});
 
   $("#space-index-filter-form").on("ajax:success", function(event, data){
     $("#space-list-wrapper").html(data);
