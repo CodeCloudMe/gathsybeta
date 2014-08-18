b5166b6a0c60a8dff5a95a5905740911d66b4b9d
ignite <ignite@kirkclarke.com>
2014-08-17 20:26:01 -0400

    testing filter js edit, with submit return

--- app/assets/javascripts/space_index_form.js
+++ app/assets/javascripts/space_index_form.js
@@ -1,36 +1,18 @@
-// JavaScript Document
-
 $(document).ready(function() {
 
   $("#space-index-filter-form").find(":input").each(function(){
     $(this).change(function(){
-      $("#space-index-filter-form").submit(function() {  
-      var valuesToSubmit = $(this).serialize();
-        $.ajax({
-          type: "POST",
-          url: "/spaces", //sumbits it to the given url of the form
-          data: valuesToSubmit,
-          dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
-        }).success(function(event, data){
-          $("#space-list-wrapper").html(data);
-            initializeMap();
-          });
-          return false; // prevents normal behaviour
-          });
+      $("#space-index-filter-form").submit();
     });
   });
 
- //  $('#space-index-filter-form').bind('submit', function(event) {
- //     return false;
- //  });
+	// $("#space-index-sidebar-filter-button").on("click", function(){
+	// 	$("#space-index-filter-form").submit();
+	// });
 
-  // $("#space-index-sidebar-filter-button").bind("click", function(){
-  //  $("#space-index-filter-form").submit();
-  // });
-
-  // $("#space-index-filter-form").on("ajax:success", function(event, data){
-  //   $("#space-list-wrapper").html(data);
-  //   initializeMap();
-  // });
+  $("#space-index-filter-form").on("ajax:success", function(event, data){
+    $("#space-list-wrapper").html(data);
+    initializeMap();
+  });
 
 });
